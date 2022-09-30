afead0e4ca6ce9916c393733aaff406e6b775118
Iqbaluddin Syam Had <iqbaluddinsh@gmail.com>
2022-09-30 14:03:20 +0700

    storeuser

--- @types/index.ts
+++ @types/index.ts
@@ -3,8 +3,14 @@ declare global {
 }
 
 export type MemberStatus = 'pending' | 'active' | 'suspend' | 'inactive';
+export type StoreStatus = 'active' | 'suspend';
 
 export interface LocationGeometry {
   type: string;
   coordinates: [number, number];
+}
+
+export interface MapCoordinates {
+  latitude: number | null;
+  longitude: number | null;
 }
