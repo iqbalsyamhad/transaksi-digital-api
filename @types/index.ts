1c45f5a91c72c6cc96d80771b28b81e064e4cb1e
Iqbaluddin Syam Had <iqbaluddinsh@gmail.com>
2022-10-02 14:50:43 +0700

    token init

--- @types/index.ts
+++ @types/index.ts
@@ -4,6 +4,8 @@ declare global {
 
 export type MemberStatus = 'pending' | 'active' | 'suspend' | 'inactive';
 export type StoreStatus = 'active' | 'suspend';
+export type ProductOrderStatus = 'paid' | 'processing' | 'canceled' | 'done';
+export type ProductOrderDetailStatus = 'paid' | 'processing' | 'canceled' | 'done';
 
 export interface LocationGeometry {
   type: string;
